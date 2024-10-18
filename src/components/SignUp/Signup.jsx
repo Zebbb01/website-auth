5ac499ce97376e2b7e45bda7643d7d2b27ee5abe
Zebbb01 <geraldvillaceran01@gmail.com>
2024-10-18 19:19:41 +0800

    last commit

--- src/components/SignUp/Signup.jsx
+++ src/components/SignUp/Signup.jsx
@@ -30,7 +30,7 @@ const Signup = ({ setIsLogin }) => {
     if (/[A-Z]/.test(password)) strength += 1;
     if (/[a-z]/.test(password)) strength += 1;
     if (/\d/.test(password)) strength += 1;
-    if (/[!@#$%^&*(),.?":{}|<>]/.test(password)) strength += 1;
+    if (/[!@#$%^&*_()-,.?":{}|<>]/.test(password)) strength += 1;
     return strength;
   };
 
